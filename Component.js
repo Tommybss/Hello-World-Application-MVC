df97ddccbe78b1813f02344f562ce72aa41859f9
Thomas Beck <thomas8beck@gmail.com>
2017-03-27 19:40:43 -0500

    Add files via upload

---
+++ Component.js
@@ -0,0 +1,29 @@
+jQuery.sap.declare("ui5.Component");
+
+sap.ui.core.UIComponent.extend("ui5.Component", {
+	metadata: {
+	},
+	
+	init: function() {
+		
+		//call createContent
+		sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
+		
+		var oModel = new sap.ui.model.json.JSONModel("model/HelloModel.json");
+        this.setModel(oModel, "hello");
+		
+	},
+	
+              createContent : function() {
+
+	    // create root view
+	    var oView = sap.ui.view({
+		id : "app",
+		viewName : "ui5.view.App",
+		type : "JS",
+		viewData : { component : this }
+	   });
+
+	    return oView;
+                }
+});
